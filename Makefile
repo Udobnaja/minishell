560723cdd89f43c30954d3bca88273fd36f8a0cf
Anna Udobnaia <audobnai@c1r1p7.hive.fi>
2025-08-22 17:58:18 +0300

    MSH-4: initial main

--- Makefile
+++ Makefile
@@ -1,1 +1,17 @@
 NAME := minishell
+
+CC := cc
+
+CFLAGS := -Wall -Wextra -Werror -I.  
+
+SRC := main.c
+
+OBJ := $(SRC:.c=.o)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) -lreadline
--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
+char *get_prompt()
+{
+	return ("minishell$");
+}
+
 int main ()
+{
+	char *line;
+
+	while(1)
+	{
+		line = readline(get_prompt());
+		if (!line)
+		{
+			return (0);
+		} else 
+		{
+			if (*line)
+    			add_history(line);
+			free(line);
+		}
+	}
+}
