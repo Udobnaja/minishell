de72622a802bf872feb01f6c19fb0aa526e41a87
Anastasia Lavrukhina <an.lavrukhina@gmail.com>
2025-09-23 15:56:35 +0300

    MSH-55: create exit function

--- errors/err_exec_msg.c
+++ errors/err_exec_msg.c
@@ -9,7 +9,8 @@ const char *err_exec_msg(int code)
 		[EXEC_ERR_INVALID_OPTION] = "export: options are not supported",
 		[EXEC_INVALID_IDENTIFIER] = "export: `%s': not a valid identifier",
 		[EXEC_ERR_GEN] = "command_name: %s", // TODO: impl %s for command
-		[EXEC_ERR_CD] = "cd: HOME is not set"
+		[EXEC_ERR_CD] = "cd: HOME is not set",
+		[EXEC_ERR_NUMERIC_ARG] = "exit: numeric argument required"
 	};
 	const int size = sizeof(map) / sizeof(*map);
 
---
+++ executor/builtins/exit.c
@@ -0,0 +1,62 @@
+#include "executor_internal.h"
+
+void exit_destroy_minishell(t_shell *sh, int status)
+{
+    if(sh)
+    {
+        if(sh->env_store)
+            env_destroy(&sh->env_store);
+        if(sh->heredoc_store)
+            heredoc_store_destroy(&sh->heredoc_store);
+        if(sh->history_path)
+        {
+            free(sh->history_path);
+            sh->history_path = NULL;
+        } 
+        rl_clear_history();      
+    }
+    exit(status);
+}
+static int exec_is_number(const char *str)
+{
+    int i;
+
+    if(!str || !*str)
+        return 0;
+    i = 0;
+    if(str[i] == '+' || str[i] == '-')
+        i++;
+    if(!str[i])
+        return 0;
+    while(str[i])
+    {
+        if(!ft_isdigit((unsigned char)str[i]))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+t_exec_status builtin_exit(t_shell *sh, t_cmd cmd)
+{
+    long long code;
+    t_err_payload payload;
+    
+    ft_putendl_fd("exit", STDOUT_FILENO);
+    if(!cmd.argv[1])
+        exit_destroy_minishell(sh, (int)(unsigned char)sh->last_status);
+    if(!exec_is_number(cmd.argv[1]))
+    {
+        payload.identifier = cmd.argv[1];
+        err_print(ERR_EXEC, EXEC_ERR_NUMERIC_ARG, payload);
+        exit_destroy_minishell(sh, 2);
+    }
+    if(cmd.argv[2])
+    {
+        err_print(ERR_EXEC, EXEC_TOO_MANY_ARGS, (t_err_payload){0});
+        return EXEC_TOO_MANY_ARGS;
+    }
+    code = ft_atoll(cmd.argv[1]);
+    exit_destroy_minishell(sh, (int)(unsigned char)code);
+    return EXEC_OK;
+}
