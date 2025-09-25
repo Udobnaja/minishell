43a75891ceec4b3e8abcc65cb7b47390e6f3094a
Anastasia Lavrukhina <an.lavrukhina@gmail.com>
2025-09-25 17:21:35 +0300

    MSH-67: add external_cmd

---
+++ executor/external_cmd.c
@@ -0,0 +1,193 @@
+#include "executor_internal.h"
+
+static t_exec_status run_builtin(t_shell *sh, const t_cmd cmd)
+{
+    const char *str;
+
+    if(cmd.argv == NULL)
+        return EXEC_NOT_BUILTIN;
+    if(cmd.argv[0] == NULL)
+        return EXEC_NOT_BUILTIN;
+    str = cmd.argv[0];
+    if(ft_strcmp(str, "pwd") == 0)
+        return pwd(sh, cmd);
+    if(ft_strcmp(str, "echo") == 0)
+        return echo(cmd);
+    if(ft_strcmp(str, "export") == 0)
+        return export(sh, cmd);
+    if(ft_strcmp(str, "unset") == 0)
+        return unset(sh, cmd);
+    if(ft_strcmp(str, "env") == 0) //добавить остальные билдины
+        return env(sh, cmd);
+    return EXEC_NOT_BUILTIN;
+}
+
+static int check_candidate(const char *dir, size_t len, const char *name, char out[PATH_MAX])
+{
+    if(dir == NULL || name == NULL || out == NULL)
+        return 0;
+    if(len + 1 + ft_strlen(name) + 1 >= PATH_MAX)
+        return 0;
+    if(len > 0)
+        ft_memcpy(out, dir, len);
+    out[len] = '/';
+    out[len + 1] = '\0';
+    ft_strlcat(out, name, PATH_MAX);
+    if(access(out, X_OK) == 0)
+        return 1;
+    return 0;
+}
+static int search_in_path(const char *path, const char *name, char out[PATH_MAX])
+{
+    size_t i;
+    size_t start;
+    size_t len;
+
+    if(path == NULL || start == NULL||len == NULL)
+        return 0;
+    i = 0;
+    start = 0;
+    while(1)
+    {
+        if(path[i] == ':' || path[i] == '\0')
+        {
+            len = i - start;
+            if(check_candidate(path+start, len, name, out) != 0)
+                return 1;
+            if(path[i] == '\0')
+                break;
+            start = i + 1;
+        }
+        if(path[i] == '\0')
+            break;
+        i = i + 1;
+    }
+    errno = ENOENT;
+    return 0;
+}
+
+static int cmd_path(t_shell *sh, const char *name, char out[PATH_MAX])
+{
+    const char *path_env;
+    if (name == NULL || out == NULL)
+        return 0;
+    if(ft_strchr(name, '/') != NULL)
+    {
+        ft_strlcpy(out, name, PATH_MAX);
+        return 1;
+    }
+    path_env = env_get_value(sh->env_store, "PATH");
+    if(path_env == NULL)
+    {
+        errno = ENOENT;
+        return 0;
+    }
+    if(path_env[0] == '\0')
+    {
+        errno = ENOENT;
+        return 0;
+    }
+    if(search_in_path(path_env, name, out) != 0)
+        return 1;
+    return 0;
+}
+
+static int preliminary_check(const char *path, const char *argv)
+{
+    struct stat st;
+    if(stat(path, &st))
+    {
+        err_print(ERR_EXEC, EXEC_NO_SUCH_FILE, 
+            (t_err_payload){.identifier = (char *)argv});
+        return X_NOTFOUND;;
+    }
+    if (S_ISDIR(st.st_mode))
+    {
+        if (ft_strchr(argv, '/') == NULL)
+        {
+            err_print(ERR_EXEC, EXEC_NO_SUCH_FILE,
+                      (t_err_payload){ .identifier = (char*)argv });
+            return X_NOTFOUND;
+        }
+        err_print(ERR_EXEC, EXEC_NO_SUCH_FILE,
+                  (t_err_payload){ .identifier = (char*)argv});
+        return X_NOEXEC;
+    }
+
+    if (access(path, X_OK) == -1)
+    {
+        if (errno == EACCES)
+        {
+            err_print(ERR_EXEC, EXEC_ERR_PERMISSION,
+                      (t_err_payload){ .identifier = (char*)argv});
+                return X_NOEXEC;
+        }
+        else
+        {
+            err_print(ERR_EXEC, EXEC_ERR_GEN,
+                      (t_err_payload){ .identifier = (char*)argv});
+                return X_NOEXEC;
+        }
+        return 0;
+    }
+
+}
+
+static t_exec_status run_external_cmd(t_shell *sh, t_cmd cmd)
+{
+    char full[PATH_MAX];
+    pid_t pid;
+    int status;
+    int temp_err_msg;
+    char **envp;
+
+    if(cmd.argv == NULL || cmd.argv[0] == NULL || cmd.argv[0][0] == '\0')
+        return EXEC_OK;
+    full[0] = '\0';
+    if(!cmd_path(sh, cmd.argv[0], full))
+    {
+        err_print(ERR_EXEC, EXEC_CMD_NOT_FOUND, (t_err_payload){.identifier = cmd.argv[0]});
+        sh->last_status = 127; // command not found
+        return EXEC_OK;
+    }
+    temp_err_msg = preliminary_check (full, cmd.argv[0]);
+    if(temp_err_msg != 0)
+    {
+        sh->last_status = temp_err_msg;
+        return EXEC_OK;
+    }
+    pid = fork();
+    if(pid < 0)
+    {
+        err_print(ERR_EXEC, EXEC_ERR_FORK, (t_err_payload){0});
+        return EXEC_ERR_FORK;
+    }
+    if(pid == 0)
+    {
+        envp = env_to_envp(sh->env_store);
+        if(envp == NULL)
+            _exit(1);
+        execve(full, cmd.argv, envp);
+        _exit(1);
+    }
+    if(waitpid(pid, &status, 0) == -1)
+    {
+        sh->last_status = 1;
+        return EXEC_ERR_WAIT;
+    }
+    if(WIFEXITED(status))
+        sh->last_status = WEXITSTATUS(status);
+    else if(WIFSIGNALED(status))
+        sh->last_status = 128 + WTERMSIG(status);
+    return EXEC_OK;
+}
+
+t_exec_status exec_external_cmd(t_shell *sh, const t_cmd cmd)
+{
+    t_exec_status status;
+
+    status = run_builtin(sh, cmd);
+    if(status != EXEC_NOT_BUILTIN)
+        return status;
+    return run_external_cmd(sh, cmd);
+}
