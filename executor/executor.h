5f5c511f3809bc1ec0fa7f61a222372dbb3a521e
Anastasia Lavrukhina <an.lavrukhina@gmail.com>
2025-09-25 17:21:35 +0300

    MSH-67: add external_cmd

--- executor/executor.h
+++ executor/executor.h
@@ -1,8 +1,14 @@
 #ifndef EXECUTOR_H
 # define EXECUTOR_H
 
+#include <sys/wait.h>
+
+# include "minishell.h"
 # include "pipeline.h"
 # include "shell.h"
+
+#define X_NOTFOUND 127
+#define X_NOEXEC 126
 
 typedef enum e_exec_status
 {
@@ -13,8 +19,14 @@ typedef enum e_exec_status
 	EXEC_INVALID_IDENTIFIER,
 	EXEC_ERR_GEN,
 	EXEC_ERR_CD,
-	EXEC_ERR_NUMERIC_ARG
-}	t_exec_status;
+	EXEC_ERR_NUMERIC_ARG,
+	EXEC_NOT_BUILTIN,
+	EXEC_NO_SUCH_FILE,
+	EXEC_CMD_NOT_FOUND,
+	EXEC_ERR_PERMISSION,
+	EXEC_ERR_FORK,
+	EXEC_ERR_WAIT
+}				t_exec_status;
 
 t_exec_status	mock_exec(t_shell *sh, t_pipeline *pipeline);
 
