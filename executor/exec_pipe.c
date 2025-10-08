d12084091c25c222862a43b30eed350f219c4eca
Anastasia Lavrukhina <an.lavrukhina@gmail.com>
2025-10-08 12:23:18 +0300

    MSH-76: pipe, reduction to 25 lines

--- executor/exec_pipe.c
+++ executor/exec_pipe.c
@@ -94,28 +94,94 @@ t_exec_status wait_all(pid_t *pids, size_t n, int *last_status_out)
     return EXEC_OK;
 }
 
-void run_child_process(t_pipe *p, size_t i)
+void exec_handle_error1(const char *path)
 {
-    t_cmd  *cmd;
-    int     st;
-    char    full[PATH_MAX];
-    char  **envp;
+    t_err_payload pay;
+    int code;
 
-    cmd = p->pl->cmds[i];
+    pay.identifier = (char *)path;
+
+    if (errno == ENOENT)
+    {
+        err_print(ERR_EXEC, EXEC_NO_SUCH_FILE, pay);
+        code = 127;
+    }
+    else if (errno == EACCES)
+    {
+        err_print(ERR_EXEC, EXEC_PERMISSION_DENIED, pay);
+        code = 126;
+    }
+    else if (errno == EISDIR)
+    {
+        err_print(ERR_EXEC, EXEC_IS_DIRECTORY, pay);
+        code = 126;
+    }
+    exit(code);
+}
+
+void exec_handle_error2(const char *path)
+{
+    t_err_payload pay;
+    int code;
+
+    pay.identifier = (char *)path;
+    if (errno == ENOEXEC)
+    {
+        err_print(ERR_EXEC, EXEC_ERR_NOT_EXEC, pay);
+        code = 126;
+    }
+    else
+    {
+        err_print(ERR_EXEC, EXEC_ERR_EXECUTION, pay);
+        code = 126;
+    }
+    exit(code);
+}
+static void child_setup(t_pipe *p, size_t i)
+{
     if (child_process(p, i) != EXEC_OK)
     {
         err_print(ERR_EXEC, EXEC_ERR_DUP, (t_err_payload){0});
         exit(1);
     }
-    if (cmd == NULL || cmd->argv == NULL || cmd->argv[0] == NULL || cmd->argv[0][0] == '\0')
-        exit(0);
+}
+
+int cmd_is_empty(const t_cmd *cmd)
+{
+    if (cmd == NULL) 
+        return 1;
+    if (cmd->argv == NULL) 
+        return 1;
+    if (cmd->argv[0] == NULL) 
+        return 1;
+    if (cmd->argv[0][0] == '\0') 
+        return 1;
+    return 0;
+}
+
+void exec_run_buildin(t_shell *sh, t_cmd *cmd)
+{
+    int st;
     if (cmd->builtin_kind != BUILTIN_NONE)
     {
-        st = execute_builtin(p->sh, cmd);
+        st = execute_builtin(sh, cmd);
         exit(st);
     }
-    full[0] = '\0';
-    if (cmd_path(p->sh, cmd->argv[0], full) == 0)
+}
+
+void run_child_process(t_pipe *p, size_t i)
+{
+    t_cmd  *cmd;
+    char    path[PATH_MAX];
+    char  **envp;
+
+    cmd = p->pl->cmds[i];
+    child_setup(p, i);
+    if (cmd_is_empty(cmd))
+        exit(0);
+    exec_run_buildin(p->sh, cmd);
+    path[0] = '\0';
+    if (cmd_path(p->sh, cmd->argv[0], path) == 0)
     {
         t_err_payload pay;
         pay.identifier = cmd->argv[0];
@@ -126,49 +192,42 @@ void run_child_process(t_pipe *p, size_t i)
     if (envp == NULL)
         exit(1);
 
-    execve(full, cmd->argv, envp);
+    execve(path, cmd->argv, envp);
+    exec_handle_error1(path);
+    exec_handle_error2(path);
+}
+
+t_exec_status exec_make_pipe(t_pipe *p)
+{
+    if(pipe(p->next) == -1)
     {
-        t_err_payload pay;
-        int code;
-
-        pay.identifier = full;
-
-        if (errno == ENOENT)
-        {
-            err_print(ERR_EXEC, EXEC_NO_SUCH_FILE, pay);
-            code = 127;
-        }
-        else if (errno == EACCES)
-        {
-            err_print(ERR_EXEC, EXEC_PERMISSION_DENIED, pay);
-            code = 126;
-        }
-        else if (errno == EISDIR)
-        {
-            err_print(ERR_EXEC, EXEC_IS_DIRECTORY, pay);
-            code = 126;
-        }
-        else if (errno == ENOEXEC)
-        {
-            err_print(ERR_EXEC, EXEC_ERR_NOT_EXEC, pay);
-            code = 126;
-        }
-        else
-        {
-            err_print(ERR_EXEC, EXEC_ERR_EXECUTION, pay);
-            code = 126;
-        }
-        exit(code);
+        err_print(ERR_EXEC, EXEC_ERR_PIPE, (t_err_payload){0});
+        pipe_close(p);
+        return EXEC_ERR_PIPE;
     }
+    return EXEC_OK;
+}
+t_exec_status exec_fork_child(t_pipe *p, size_t i)
+{
+    p->pids[i] = fork();
+    if(p->pids[i] == -1)
+    {
+        err_print(ERR_EXEC, EXEC_ERR_FORK, (t_err_payload){0});
+        pipe_close(p);
+        return EXEC_ERR_FORK;
+    }
+    if(p->pids[i] == 0)
+        run_child_process(p, i);
+    return EXEC_OK;
 }
 
 t_exec_status execution_run_pipeline(t_pipe *p)
 {
     size_t i;
+
+    i = 0;
     p->prev[FD_READ] = -1;
     p->prev[FD_WRITE] = -1;
-    
-    i = 0;
     while(i < p->n)
     {
         p->next[FD_READ]  = -1;
@@ -176,74 +235,61 @@ t_exec_status execution_run_pipeline(t_pipe *p)
 
         if (i + 1 < p->n)
         {
-            if (pipe(p->next) == -1)
-            {
-                err_print(ERR_EXEC, EXEC_ERR_PIPE, (t_err_payload){0});
-                pipe_close(p);
+            if (exec_make_pipe(p) != EXEC_OK)
                 return EXEC_ERR_PIPE;
-            }
         }
-
-        p->pids[i] = fork();
-        if (p->pids[i] == -1)
-        {
-            err_print(ERR_EXEC, EXEC_ERR_FORK, (t_err_payload){0});
-            pipe_close(p);
+        if (exec_fork_child(p, i) != EXEC_OK)
             return EXEC_ERR_FORK;
-        }
-
         if (p->pids[i] == 0)
             run_child_process(p, i);
 
         pipe_parent_rotate(p);
         i++;
     }
-    close_fd(&p->prev[FD_READ]);
-    close_fd(&p->prev[FD_WRITE]);
-    close_fd(&p->next[FD_READ]);
-    close_fd(&p->next[FD_WRITE]);
+    pipe_close(p);
     return EXEC_OK;
 }
-
-t_exec_status execute_pipeline(t_shell *sh, t_pipeline *pl)
+t_exec_status run_full_pipeline(t_shell *sh, t_pipeline *pl)
 {
-    t_pipe         p;
-    int            last = 0;
-    t_exec_status  st;
+    t_pipe p;
+    int last = 0;
+    t_exec_status st;
 
-    if (pl == NULL || pl->count == 0)
-    {
-        sh->last_status = 0;
-        return EXEC_OK;
-    }
-    if (pl->count == 1 && pl->cmds[0] != NULL
-        && pl->cmds[0]->builtin_kind != BUILTIN_NONE)
-    {
-        sh->last_status = execute_builtin(sh, pl->cmds[0]);
-        return EXEC_OK;
-    }
-
-    p.sh   = sh;
-    p.pl   = pl;
-    p.n    = pl->count;
-    p.pids = (pid_t *)malloc(sizeof(pid_t) * p.n);
-    if (p.pids == NULL)
+    p.sh = sh;
+    p.pl = pl;
+    p.n = pl->count;
+    p.pids = (pid_t *)malloc(sizeof(pid_t)* p.n);
+    if(p.pids == NULL)
     {
         err_print(ERR_EXEC, EXEC_ALLOC_ERR, (t_err_payload){0});
         sh->last_status = 1;
         return EXEC_ALLOC_ERR;
     }
-
     st = execution_run_pipeline(&p);
-    if (st == EXEC_OK)
+    if(st == EXEC_OK)
     {
-        if (wait_all(p.pids, p.n, &last) != EXEC_OK)
+        if(wait_all(p.pids, p.n, &last) != EXEC_OK)
             last = 1;
     }
-    else
+    else 
         last = 1;
-
     free(p.pids);
     sh->last_status = last;
     return st;
+}
+
+t_exec_status execute_pipeline(t_shell *sh, t_pipeline *pl)
+{
+    if(pl == NULL || pl->count == 0)
+    {
+        sh->last_status = 0;
+        return EXEC_OK;
+    }
+    if(pl->count == 1 && pl->cmds[0] != NULL
+        && pl->cmds[0]->builtin_kind != BUILTIN_NONE)
+    {
+        sh->last_status = execute_builtin(sh, pl->cmds[0]);
+        return EXEC_OK;
+    }
+    return run_full_pipeline(sh, pl);
 }
