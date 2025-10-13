26bb01e7ec18672dbe01f0fe9f17866fbd84e6f8
Anna Udobnaja <udobnaja@gmail.com>
2025-10-13 22:16:17 +0300

    MSH-82: clean name

--- executor/builtins/cd.c
+++ executor/builtins/cd.c
@@ -1,6 +1,6 @@
 #include "executor_internal.h"
 
-static t_exec_status cd_print_and_exit_error(t_exec_status status, char *cmd, int errno_val)
+static t_exec_status cd_print_error_and_exit(t_exec_status status, char *cmd, int errno_val)
 {
 	t_err_payload payload;
 	
@@ -21,21 +21,21 @@ static t_exec_status cd_go_path(t_shell *sh, const char *target)
 	oldpwd = u_getcwd();
 	if(chdir(target) != 0)
 		return (
-			cd_print_and_exit_error(EXEC_ERR_GEN, "cd", errno)
+			cd_print_error_and_exit(EXEC_ERR_GEN, "cd", errno)
 		);
 	if(oldpwd && env_set(sh->env_store, "OLDPWD", oldpwd) != ENV_OK)
 		return (
-			cd_print_and_exit_error(EXEC_ALLOC_ERR, NULL, 0)
+			cd_print_error_and_exit(EXEC_ALLOC_ERR, NULL, 0)
 		);
 	errno = 0;
 	newpwd = u_getcwd();
 	if(!newpwd)
 		return (
-			cd_print_and_exit_error(EXEC_ERR_GEN, "cd", errno)
+			cd_print_error_and_exit(EXEC_ERR_GEN, "cd", errno)
 		);
 	if(env_set(sh->env_store, "PWD", newpwd) != ENV_OK)
 		return (
-			cd_print_and_exit_error(EXEC_ALLOC_ERR, NULL, 0)
+			cd_print_error_and_exit(EXEC_ALLOC_ERR, NULL, 0)
 		);
 	return EXEC_OK;
 }
@@ -46,7 +46,7 @@ static t_exec_status cd_go_home(t_shell *sh)
 	
 	if(!home)
 		return (
-			cd_print_and_exit_error(EXEC_ERR_CD, NULL, 0)
+			cd_print_error_and_exit(EXEC_ERR_CD, NULL, 0)
 		);
 	return cd_go_path(sh, home);
 }
@@ -58,11 +58,11 @@ t_exec_status cd(t_shell *sh, t_cmd *cmd)
 		return cd_go_home(sh);
 	if (exec_is_invalid_option(cmd->argv[1]))
 		return (
-			cd_print_and_exit_error(EXEC_ERR_INVALID_OPTION, cmd->argv[0], 0)
+			cd_print_error_and_exit(EXEC_ERR_INVALID_OPTION, cmd->argv[0], 0)
 		);
 	if(cmd->argv[2])
 		return (
-			cd_print_and_exit_error(EXEC_TOO_MANY_ARGS, cmd->argv[0], 0)
+			cd_print_error_and_exit(EXEC_TOO_MANY_ARGS, cmd->argv[0], 0)
 		);
 	return cd_go_path(sh, cmd->argv[1]);
 }
