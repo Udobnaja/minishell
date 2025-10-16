4c5385a548ac5ec59dbe418fd29bf4bf3fa7ff26
Anna Udobnaja <Udobnaja@users.noreply.github.com>
2025-10-16 16:35:15 +0300

    MSH-94: PIPLENIE_REFACTOR (#96)
    * MSH-94: Pipeline refactor
    
    * MSH-94: pipe
    
    * MSH-94: double free
    
    * MSH-94: pipe

--- executor/executor_internal.h
+++ executor/executor_internal.h
@@ -26,10 +26,14 @@ t_exec_result	builtin_exit(t_shell *sh, t_cmd *cmd);
 
 t_exec_result   execute_external(t_shell *sh, t_cmd *cmd);
 t_exec_result   execute_builtin(t_shell *sh, t_cmd *cmd);
-t_exec_status   execute_pipeline(t_shell *sh, t_pipeline *pl);
-t_exec_status   execute_pipeline(t_shell *sh, t_pipeline *pl);
+t_exec_result   execute_pipeline(t_shell *sh, t_pipeline *pl);
 t_exec_status   apply_redirections(t_cmd *cmd);
 int             cmd_path(t_shell *sh, const char *name, char out[PATH_MAX]);
+
+t_exec_result    exec_external_error_result(t_exec_status status, const char *cmd, int errno_val);
+t_exec_result    exec_external_sys_error(t_exec_status status, const char *cmd, int errno_val);
+t_exec_result    exec_external_result(t_exec_status status, int exit_code);
+void             exec_child(const char *full, t_cmd *cmd, t_shell *sh);
 
 
 #endif
