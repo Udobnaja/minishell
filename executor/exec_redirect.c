fa4b66f5cd4139f5f6156435a8ad253996141357
LavrAnastasia <an.lavrukhina@gmail.com>
2025-10-21 12:59:20 +0300

    MSH-77: Redirects (#92)
    * MSH-67: fixed external_cmd -> in err_print EXEC_ERR_EXECUTION
    
    * MSH-77: delete pipe_child_close
    
    MSH-77: basic redirect works
    
    MSH-77: added apply reirection in external
    
    MSH-77: Ana added result for function
    
    MSH-77:delete identifier
    
    MSH-77: changes before t_exec_result and err_print
    
    * MSH-77: made a litle fix for compile
    
    * MSH-77: delete fd < 0 in apply_redirection

--- executor/exec_redirect.c
+++ executor/exec_redirect.c
@@ -25,17 +25,18 @@ int select_target(const t_redirect *r)
 }
 t_exec_status dup2_end_close(int fd, int target, const char *name)
 {
-    int err;
+    t_err_payload payload;
 
     if(fd == target)
         return EXEC_OK;
     if(dup2(fd, target) < 0)
     {
-        err = errno;
         close(fd);
-        t_exec_status st = redir_status_from_errno(err);
-        err_print(ERR_EXEC, st, (t_err_payload){.identifier = (char *)name}); //identifier? чтобы вывести какой файл выдал ошибку 
-        return st;
+        payload.command = name;
+        payload.errno_val = errno;
+
+        err_print(ERR_EXEC, EXEC_ERR_GEN, payload);
+        return (EXEC_ERR_GEN);
     }
     close (fd);
     return EXEC_OK;
@@ -54,35 +55,26 @@ t_exec_status open_for_redirect(const t_redirect *r, int *out_fd)
         fd = open(r->target.path, O_WRONLY | O_CREAT | O_APPEND, 0644);
     else if (r->type == REDIR_HEREDOC)
         fd = r->target.fd;
-    *out_fd = fd;
     if (fd < 0)
         return redir_status_from_errno(errno);
+    *out_fd = fd;
     return EXEC_OK;
 }
 
 t_exec_status apply_one_redirect(const t_redirect *r)
 {
-    t_exec_status st;
+    t_exec_status status;
     int fd;
     int target;
 
     if(r->type == REDIR_HEREDOC)
-    {
-        fd = r->target.fd;
-        if(fd < 0)
-        {
-            err_print(ERR_EXEC, EXEC_ERR_PERMISSION, 
-                (t_err_payload){.identifier = NULL});
-                return EXEC_ERR_PERMISSION;
-        }
-        return dup2_end_close(fd, STDIN_FILENO, NULL);
-    }
-    st = open_for_redirect(r, &fd);
-    if(st != EXEC_OK)
-    {
-        err_print(ERR_EXEC, st,
-            (t_err_payload){ .identifier = r->target.path });
-        return st;
+        return dup2_end_close(r->target.fd, STDIN_FILENO, NULL);
+    status = open_for_redirect(r, &fd);
+    if(status != EXEC_OK)
+    { // тут печать либо выше либо возарщать еррно и печатать гену
+        err_print(ERR_EXEC, status,
+            (t_err_payload){ .command = r->target.path });
+        return status;
     }
     target = select_target(r);
     return dup2_end_close(fd, target, r->target.path);
@@ -92,16 +84,16 @@ t_exec_status apply_one_redirect(const t_redirect *r)
 t_exec_status apply_redirections(t_cmd *cmd)
 {
     t_redirect *r;
-    t_exec_status st;
+    t_exec_status status;
 
     if(cmd == NULL)
         return EXEC_OK;
     r = cmd->redirect_list;
     while(r != NULL)
     {
-        st = apply_one_redirect(r);
-        if(st != EXEC_OK)
-            return st;
+        status = apply_one_redirect(r);
+        if(status != EXEC_OK)
+            return status;
         r = r->next;
     }
     return EXEC_OK;
