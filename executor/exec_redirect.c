cbba986d7410dea3123b4597eda6e8f198f1d4c9
Anna Udobnaja <Udobnaja@users.noreply.github.com>
2025-10-23 20:35:26 +0300

    MSH-redirs: spply redirects (#120)
    Co-authored-by: Anna Udobnaia <audobnai@c1r2p1.hive.fi>

--- executor/exec_redirect.c
+++ executor/exec_redirect.c
@@ -23,35 +23,6 @@ int select_target(const t_redirect *r)
         return STDERR_FILENO;
     return STDOUT_FILENO;
 }
-t_exec_result exec_redirect_result(t_exec_status status, int exit_code)
-{
-    t_exec_result result;
-
-    result = (t_exec_result){0};
-	result.flow = FLOW_OK;
-	result.exit_code = (int)(unsigned char)exit_code;
-	result.status = status;
-	result.errno_val = 0;
-	return (result);
-}
-
-t_exec_result    exec_redirect_error(t_exec_status status, const char *name, int errno_val)
-{
-	t_exec_result	result;
-	t_err_payload	payload;
-
-    result = (t_exec_result){0};
-    payload = (t_err_payload){0};
-    result.flow = FLOW_OK;
-	if (status == EXEC_ERR_GEN || status == EXEC_NO_SUCH_FILE || status == EXEC_ERR_PERMISSION)	
-	result.status = status;
-    result.exit_code = SH_GENERAL_ERROR;
-
-	payload.command = name;
-    result.errno_val = errno_val;
-	err_print(ERR_EXEC, status, payload);
-	return (result);
-}
 
 t_exec_result dup2_end_close(int fd, int target, const char *name)
 {
@@ -59,16 +30,16 @@ t_exec_result dup2_end_close(int fd, int target, const char *name)
     t_exec_status status;
 
     if(fd == target)
-        return exec_redirect_result(EXEC_OK, 0);
+        return exec_external_result(EXEC_OK, SH_OK);
     if(dup2(fd, target) < 0)
     {
         error = errno;
         close(fd);
         status = redir_status_from_errno(error);
-        return exec_redirect_error(status, name, error);
+        return exec_external_sys_error(status, name, error);
     }
     close (fd);
-    return exec_redirect_result(EXEC_OK, 0);
+    return exec_external_result(EXEC_OK, SH_OK);
 }
 
 t_exec_status open_for_redirect(const t_redirect *r, int *out_fd)
@@ -95,16 +66,12 @@ t_exec_result apply_one_redirect(const t_redirect *r)
     t_exec_status status;
     int fd;
     int target;
-    int error;
 
     if(r->type == REDIR_HEREDOC)
         return dup2_end_close(r->target.fd, STDIN_FILENO, NULL);
     status = open_for_redirect(r, &fd);
     if(status != EXEC_OK)
-    {
-        error = errno;
-        return exec_redirect_error(EXEC_ERR_GEN, r->target.path, error);
-    }
+        return exec_external_sys_error(EXEC_ERR_GEN, r->target.path, errno);
     target = select_target(r);
     return dup2_end_close(fd, target, r->target.path);
 
@@ -116,7 +83,7 @@ t_exec_result apply_redirections(t_cmd *cmd)
     t_exec_result result;
 
     if(cmd == NULL)
-        return exec_redirect_result(EXEC_OK, 0);
+        return exec_external_result(EXEC_OK, SH_OK);;
     r = cmd->redirect_list;
     while(r != NULL)
     {
@@ -125,5 +92,5 @@ t_exec_result apply_redirections(t_cmd *cmd)
             return result;
         r = r->next;
     }
-    return exec_redirect_result(EXEC_OK, 0);
+    return exec_external_result(EXEC_OK, SH_OK);
 }
