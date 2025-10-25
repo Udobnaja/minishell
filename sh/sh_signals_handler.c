f79fa753060cece3f751d7296be080eefb7d3d39
Anna Udobnaja <udobnaja@gmail.com>
2025-10-25 12:09:17 +0300

    MSH-139: Reduce func in sh folder

--- sh/sh_signals_handler.c
+++ sh/sh_signals_handler.c
@@ -1,17 +1,16 @@
 #include "shell.h"
 
-static void sh_core_dump_reporter(int wst);
+static void	sh_core_dump_reporter(int wst);
 
-
-void sh_sigint_sighandler(int signo)
+void	sh_sigint_sighandler(int signo)
 {
-    g_last_signal = signo;
-    rl_done = 1;
+	g_last_signal = signo;
+	rl_done = 1;
 }
 
-void sh_set_signal_handler(int sig, void (*handler)(int))
+void	sh_set_signal_handler(int sig, void (*handler)(int))
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
@@ -19,17 +18,17 @@ void sh_set_signal_handler(int sig, void (*handler)(int))
 	sigaction(sig, &sa, NULL);
 }
 
-void sh_report_wait_signal(int wst)
+void	sh_report_wait_signal(int wst)
 {
 	if (!WIFSIGNALED(wst))
-		return;
-
+		return ;
 	if (WTERMSIG(wst) == SIGQUIT)
 		sh_core_dump_reporter(wst);
 }
 
 #ifdef WCOREDUMP
-static void sh_core_dump_reporter(int wst)
+
+static void	sh_core_dump_reporter(int wst)
 {
 	if (WCOREDUMP(wst))
 		write(STDERR_FILENO, "Quit (core dumped)\n", 19);
@@ -37,9 +36,10 @@ static void sh_core_dump_reporter(int wst)
 		write(STDERR_FILENO, "Quit\n", 5);
 }
 #else
-static void sh_core_dump_reporter(int wst)
+
+static void	sh_core_dump_reporter(int wst)
 {
-	(void)wst;;
+	(void)wst;
 	write(STDERR_FILENO, "Quit\n", 5);
 }
 #endif
