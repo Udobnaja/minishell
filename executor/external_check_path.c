27323a3d2a2a80e1951b7a567269b985c1aab220
Anastasia Lavrukhina <an.lavrukhina@gmail.com>
2025-10-27 22:43:53 +0200

    MSH-150: split external file

--- executor/external_check_path.c
+++ executor/external_check_path.c
@@ -17,7 +17,7 @@ static int	exec_check_access(char out[PATH_MAX])
 	return (0);
 }
 
-static int	check_candidate(const char *dir, size_t len, const char *name,
+int	check_candidate(const char *dir, size_t len, const char *name,
 		char out[PATH_MAX])
 {
 	size_t	name_len;
@@ -79,26 +79,6 @@ static int	search_in_path(const char *path, const char *name,
 	return (0);
 }
 
-static int	exec_is_dot_or_dotdot(const char *s)
-{
-	return (s && (ft_strcmp(s, ".") == 0 || ft_strcmp(s, "..") == 0));
-}
-
-static int	exec_check_in_curr_dir(const char *name, char out[PATH_MAX],
-		const char *path)
-{
-	struct stat	st;
-
-	if (check_candidate(".", 1, name, out))
-		return (1);
-	if (path == NULL)
-	{
-		if (stat(out, &st) == 0)
-			return (1);
-	}
-	errno = ENOENT;
-	return (0);
-}
 /*
 The function searches for the name command and writes
 the full path to out. Decides where to search (in PATH or in a given path)
@@ -128,31 +108,6 @@ int	cmd_path(t_shell *sh, const char *name, char out[PATH_MAX])
 		return (1);
 	errno = ENOENT;
 	return (0);
-}
-
-/*
-The function checks whether the file at the specified path can be run
-path - full path to the file ("/bin/cat")
-argv - command name to print in the error message
- */
-
-t_exec_result	preliminary_check(const char *path, char *argv)
-{
-	if (!u_file_exists(path))
-		return (exec_external_error_result(EXEC_NO_SUCH_FILE, argv, 0));
-	if (u_file_isdir(path))
-	{
-		return (exec_external_error_result(EXEC_IS_DIRECTORY, argv, 0));
-	}
-	errno = 0;
-	if (access(path, X_OK) == -1)
-	{
-		if (errno)
-			return (exec_external_error_result(EXEC_ERR_GEN, argv, errno));
-		else
-			return (exec_external_error_result(EXEC_ERR_EXECUTION, NULL, 0));
-	}
-	return (exec_external_result(EXEC_OK, 0));
 }
 
 t_exec_result	external_path(t_shell *sh, const char *name,
