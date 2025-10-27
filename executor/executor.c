6a7a538bacf691ad3f3ef4a80bf7e0b166dea6f5
Anna Udobnaja <udobnaja@gmail.com>
2025-10-27 21:01:04 +0200

    MSH-138: isatty

--- executor/executor.c
+++ executor/executor.c
@@ -101,7 +101,7 @@ t_exec_result	execute(t_shell *sh, t_pipeline *pipeline)
 			return (result);
 		}
 		exec_update_underscore(sh, pipeline->cmds[pipeline->count - 1]);
-		if (pipeline->cmds[0]->builtin_kind == BUILTIN_EXIT)
+		if (pipeline->cmds[0]->builtin_kind == BUILTIN_EXIT && (isatty(STDIN_FILENO)))
 			ft_putendl_fd("exit", STDERR_FILENO);
 		return (exec_builtin_with_redirs(sh, pipeline->cmds[0]));
 	}
