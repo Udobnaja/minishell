d9289cf88de898195d08d080974ef1665eb0e009
Anna Udobnaia <audobnai@c2r4p8.hive.fi>
2025-10-28 17:50:07 +0200

    MSH-PREMOCK: size

--- heredoc/heredoc_prompt.c
+++ heredoc/heredoc_prompt.c
@@ -6,7 +6,7 @@
 /*   By: audobnai <audobnai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 21:59:22 by audobnai          #+#    #+#             */
-/*   Updated: 2025/10/27 21:59:23 by audobnai         ###   ########.fr       */
+/*   Updated: 2025/10/28 17:47:10 by audobnai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	heredoc_read_prompt(char *line, t_heredoc_status *status, char *eof)
 	if (!line)
 		return (0);
 	str_len = ft_strlen(line);
-	if (line[str_len - 1] == '\n' && !isatty(STDIN_FILENO))
+	if (str_len && line[str_len - 1] == '\n' && !isatty(STDIN_FILENO))
 		line[str_len - 1] = '\0';
 	if (g_last_signal == SIGINT)
 	{
